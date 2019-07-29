da3ae8f14f87154f6bda36dfcdb4442433dc46fe
Leonardo Larrea <leonardo@shippify.co>
2019-07-29 03:51:14 -0500

    datalistener

--- datalistener.js
+++ datalistener.js
@@ -1,1 +1,49 @@
-console.log('DATALISTENER READY!!!');
+const kue = require('kue');
+const cluster = require('cluster');
+
+if(cluster.isMaster) {
+    const numWorkers = (process.env.SERVER_ENV === "production") ?
+      require('os').cpus().length :
+      1;
+
+    console.log('Master cluster setting up ' + numWorkers + ' workers...')
+
+    for(let i = 0; i < numWorkers; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('online', (worker)=>{
+        console.log('Worker ' + worker.process.pid + ' is online')
+    })
+
+    cluster.on('exit', (worker, code, signal)=>{
+        console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal)
+        console.log('Starting a new worker')
+        cluster.fork()
+    })
+
+}else{
+    
+    const queue = kue.createQueue({
+        redis: {
+          port: process.env.JOB_QUEUE_REDIS_PORT || 6379,
+          host: process.env.JOB_QUEUE_REDIS_HOST || 'localhost',
+          auth: process.env.JOB_QUEUE_REDIS_PASSWORD
+        }
+    });
+
+    
+    queue.process('DataProcess.datalistener', 5, (job, done) => {
+
+        const domain = require('domain').create();
+
+        domain.on('error', (jobError) => {
+            console.log(jobError);
+            done(jobError);
+        });
+
+        domain.run(() => {
+            done(null)
+        });
+    });
+}
